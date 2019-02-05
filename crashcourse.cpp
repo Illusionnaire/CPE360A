c781a6e334e5e9f348d6ed1d2f6044774827319b
Marcin <marcinrwisniowski@gmail.com>
2019-02-05 11:24:20 -0500

    Created first lecture file and added .gitignore to running programs.

---
+++ crashcourse.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    float y;
+
+    cout << "Hello World" << endl;
+    cout << "Enter me a number: ";
+    cin >> x;
+
+    x = x + 10;
+
+    cout << "Here's your value plus 10: " << x << endl;
+
+    return 0;
+}
