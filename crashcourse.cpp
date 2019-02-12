8abf167f8014e20f94cbca3ab69022a2f627ec03
Marcin <marcinrwisniowski@gmail.com>
2019-02-11 19:41:38 -0500

    Used crashcourse.cpp as a way to test big-O values for homework. Used counter and sum to keep track of how many times each nested loop ran

--- crashcourse.cpp
+++ crashcourse.cpp
@@ -3,8 +3,30 @@ using namespace std;
 
 //Signature of a Function --> Must have a unique signature
 //<return-type> <name> <arguments>
-    
+
+
 int main(){
+    int counter = 0;
+    int counter2 = 0;
+    int sum = 0;
+    int n;
+    cin >> n;
+    
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < i*i; j++){
+                
+            for(int k = 0; k < j; k++){
+                counter++; // n times 1+1+1+1
+                sum+=2;
+            }
+            counter2++;
+            sum++;
+        }
+        sum++;
+    }
+
+    cout << sum << "\t" << counter2 << "\t" << counter;
+
 
     return 0;
 }
